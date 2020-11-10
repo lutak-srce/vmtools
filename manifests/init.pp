4b11ae4d65e22d3009b2d91024b7d858a041f97e
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-11-10 05:24:07 +0100

    Replace vmware tools module

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,13 @@ class vmtools {
     'ovirt':    { include ::vmtools::ovirt }
     /^xen*/:    { include ::vmtools::xen }
     /^vmware$/: { 
-                  class{'admintools::make': } ->
-                  class{'vmwaretools'     : } 
-                }
+      exec { 'vmware-uninstall-tools':
+        command => '/usr/bin/vmware-uninstall-tools.pl && rm -rf /usr/lib/vmware-tools',
+        path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+        onlyif  => 'test -f /usr/bin/vmware-uninstall-tools.pl',
+      }
+
+      class{ 'openvmtools':
+        require => Exec['vmware-uninstall-tools'],
   }
 }
