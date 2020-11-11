dfdcab038d200b0e93c8b3182134224c56f53fc2
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-11-11 11:52:22 +0100

    Replace vmwaretools module with openvmtools

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,9 @@
 # Installs the Vm tools, depending on type of virtualization
 class vmtools {
   case $::virtual {
-    default:    {}
-    'ovirt':    { include ::vmtools::ovirt }
-    /^xen*/:    { include ::vmtools::xen }
-    /^vmware$/: { 
-                  class{'admintools::make': } ->
-                  class{'vmwaretools'     : } 
-                }
+    default   : {}
+    'ovirt'   : { include vmtools::ovirt }
+    /^xen*/   : { include vmtools::xen }
+    /^vmware$/: { include openvmtools }
   }
 }
